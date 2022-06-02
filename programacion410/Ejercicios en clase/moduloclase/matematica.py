7fe14a5fe8abbbf52c4ff495e8d736eb064b39ab
eivan <102188631+ivankosz@users.noreply.github.com>
2022-06-02 19:16:54 -0300

    Add files via upload

---
+++ programacion410/Ejercicios en clase/moduloclase/matematica.py
@@ -0,0 +1,14 @@
+"""modulo de operaciones matematicas basicas
+suma, resta, multipicacion y division"""
+
+def suma(a,b): #retorna la suma de dos numeros
+    return a+b
+
+def resta(a,b): #retorna la resta de dos numeros
+    return a-b
+
+def multiplicacion(a,b): #retorna la multiplicacion de dos numeros
+    return a*b
+
+def division(a,b): #retorna la division de dos numeros
+    return a/b
---
+++ programacion410/Ejercicios en clase/moduloclase/modulo_clase.py
@@ -0,0 +1,11 @@
+"""Breve descripción del módulo
+Puede contener instrucciones de como se usa
+"""
+
+def suma(a, b):
+	""" Retorna la suma de dos operandos enviados por parámetro
+	"""
+	return a + b
+
+if (__name__ == '__main__'):
+	print(f"La suma es {suma(3,99)}") # Código "suelto"
