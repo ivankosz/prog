1b973ed15067c19cc2063a5b882bff8e7a292d9f
eivan <102188631+ivankosz@users.noreply.github.com>
2022-06-02 19:16:09 -0300

    Add files via upload

---
+++ programacion410/Ejercicios en clase/2jun.py
@@ -0,0 +1,9 @@
+from moduloclase.matematica import *
+
+n1=int(input("Ingrese un numero: "))
+n2=int(input("Ingrese otro numero: "))
+
+print(f'La suma es: {suma(n1,n2)}')
+print(f'La resta es: {resta(n1,n2)}')
+print(f'La multiplicacion es: {multiplicacion(n1,n2)}')
+print(f'La division es: {division(n1,n2)}')
